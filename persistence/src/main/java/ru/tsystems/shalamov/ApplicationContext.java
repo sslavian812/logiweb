086788804de36e610a24f226388976261d41a771
sslavian812 <sslavian812@yandex.ru>
2015-07-13 02:04:47 +0300

    Application context refactoring

--- persistence/src/main/java/ru/tsystems/shalamov/ApplicationContext.java
+++ persistence/src/main/java/ru/tsystems/shalamov/ApplicationContext.java
@@ -45,84 +45,95 @@ public final class ApplicationContext {
     public static final String ROLE_DRIVER = "driver";
 
 
-    private EntityManagerFactory entityManagerFactory;
-    private EntityManager entityManager;
+    private volatile EntityManagerFactory entityManagerFactory;
+    private volatile EntityManager entityManager;
 
-    private CargoDao cargoDao;
-    private DriverDao driverDao;
-    private DriverStatusDao driverStatusDao;
-    private OrderDao orderDao;
-    private ShiftDao shiftDao;
-    private TruckDao truckDao;
+    private volatile CargoDao cargoDao;
+    private volatile DriverDao driverDao;
+    private volatile DriverStatusDao driverStatusDao;
+    private volatile OrderDao orderDao;
+    private volatile ShiftDao shiftDao;
+    private volatile TruckDao truckDao;
 
 
-    private DriverActivityService driverActivityService;
-    private DriverAssignmentService driverAssignmentService;
-    private DriverManagementService driverManagementService;
-    private OrderManagementService orderManagementService;
-    private TruckManagementService truckManagementService;
+    private volatile DriverActivityService driverActivityService;
+    private volatile DriverAssignmentService driverAssignmentService;
+    private volatile DriverManagementService driverManagementService;
+    private volatile OrderManagementService orderManagementService;
+    private volatile TruckManagementService truckManagementService;
 
 
     private ApplicationContext() {
     }
 
-    private synchronized void createEmfInstance() {
-        if (entityManagerFactory == null) {
-            entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
-        }
-    }
-
     public EntityManagerFactory getEntityManagerFactory() {
         if (entityManagerFactory == null) {
-            createEmfInstance();
+            entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
         }
         return entityManagerFactory;
     }
 
     public EntityManager getEntityManager() {
-        if (entityManager == null || !entityManager.isOpen()) {
-            entityManager = getEntityManagerFactory().createEntityManager();
-        }
+        if (entityManager == null || !entityManager.isOpen())
+            synchronized (this) {
+                if (entityManager == null || !entityManager.isOpen()) {
+                    entityManager = getEntityManagerFactory().createEntityManager();
+                }
+            }
         return entityManager;
     }
 
     public CargoDao getCargoDao() {
-        if (cargoDao == null) {
-            cargoDao = new CargoDaoImpl(getEntityManager());
+        if (cargoDao == null) synchronized (this) {
+            if (cargoDao == null) {
+                cargoDao = new CargoDaoImpl(getEntityManager());
+            }
         }
         return cargoDao;
     }
 
     public DriverDao getDriverDao() {
-        if (driverDao == null) {
-            driverDao = new DriverDaoImpl(getEntityManager());
+        if (driverDao == null) synchronized (this) {
+            if (driverDao == null) {
+                driverDao = new DriverDaoImpl(getEntityManager());
+            }
         }
         return driverDao;
     }
 
     public ShiftDao geShiftDao() {
-        if (shiftDao == null) {
-            shiftDao = new ShiftDaoImpl(getEntityManager());
+        if (shiftDao == null) synchronized (this) {
+            if (shiftDao == null) {
+                shiftDao = new ShiftDaoImpl(getEntityManager());
+            }
         }
         return shiftDao;
     }
 
     public TruckDao getTruckDao() {
-        if (truckDao == null) {
-            truckDao = new TruckDaoImpl(getEntityManager());
+        if (truckDao == null) synchronized (this) {
+            if (truckDao == null) {
+                truckDao = new TruckDaoImpl(getEntityManager());
+            }
         }
         return truckDao;
     }
 
     public DriverStatusDao getDriverStatusDao() {
-        if (driverStatusDao == null)
-            driverStatusDao = new DriverStatusDaoImpl(getEntityManager());
+        if (driverStatusDao == null) synchronized (this) {
+            if (driverStatusDao == null) {
+                driverStatusDao = new DriverStatusDaoImpl(getEntityManager());
+            }
+        }
         return driverStatusDao;
     }
 
     public OrderDao getOrderDao() {
-        if (orderDao == null)
-            orderDao = new OrderDaoImpl(getEntityManager());
+        if (orderDao == null) synchronized (this) {
+            if (orderDao == null) {
+                orderDao = new OrderDaoImpl(getEntityManager());
+            }
+        }
         return orderDao;
     }
 
@@ -132,30 +143,43 @@ public final class ApplicationContext {
     }
 
     public DriverAssignmentService getDriverAssignmentService() {
-        if (driverAssignmentService == null)
-            driverAssignmentService = new DriverAssignmentServiceImpl(
-                    getDriverDao(), getOrderDao(), getEntityManager());
+        if (driverAssignmentService == null) synchronized (this) {
+            if (driverAssignmentService == null) {
+                driverAssignmentService = new DriverAssignmentServiceImpl(
+                        getDriverDao(), getOrderDao(), getEntityManager());
+            }
+        }
         return driverAssignmentService;
     }
 
     public DriverManagementService getDriverManagementService() {
-        if (driverManagementService == null)
-            driverManagementService = new DriverManagementServiceImpl(
-                    getDriverDao(), getDriverStatusDao(), getEntityManager());
+        if (driverManagementService == null) synchronized (this) {
+            if (driverManagementService == null) {
+                driverManagementService = new DriverManagementServiceImpl(
+                        getDriverDao(), getDriverStatusDao(), getEntityManager());
+            }
+        }
         return driverManagementService;
     }
 
     public OrderManagementService getOrderManagementService() {
-        if (orderManagementService == null)
-            orderManagementService = new OrderManagementServiceImpl(
-                    getDriverDao(), getOrderDao(), getTruckDao(),
-                    getDriverStatusDao(), getCargoDao(), getEntityManager());
+        if (orderManagementService == null) synchronized (this) {
+            if (orderManagementService == null) {
+                orderManagementService = new OrderManagementServiceImpl(
+                        getDriverDao(), getOrderDao(), getTruckDao(),
+                        getDriverStatusDao(), getCargoDao(), getEntityManager());
+            }
+        }
         return orderManagementService;
     }
 
     public TruckManagementService getTruckManagementService() {
-        if (truckManagementService == null)
-            truckManagementService = new TruckManagementServiceImpl(getTruckDao(), getEntityManager());
+        if (truckManagementService == null) synchronized (this) {
+            if (truckManagementService == null) {
+                truckManagementService = new TruckManagementServiceImpl(
+                        getTruckDao(), getEntityManager());
+            }
+        }
         return truckManagementService;
     }
 }
